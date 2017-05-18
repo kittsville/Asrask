b497959ec46ceb37955a4b18bc93345000ebe835
Kit <kittsville@googlemail.com>
2017-05-18 17:36:23 +0100

    Replace activity counter with list
    The activity manager no longer keeps track of just how many activities
    are running. It now stores the ID of every ongoing activity along with a
    callback to stop that activity.

--- _assets/scripts/app/activity.js
+++ _assets/scripts/app/activity.js
@@ -1,31 +1,52 @@
 class activityIndicator {
 	constructor(activitySpinner) {
 		this.spinner    = activitySpinner;
-		this.activities = 0;
+		this.nextId     = 0;
+		this.activities = {};
+		this.count      = 0;
 		this.spinning   = false;
 	}
 	
 	updateSpinner() {
-		if (this.spinning && this.activities == 0) {
+		if (this.spinning && this.count == 0) {
 			this.spinner.classList.remove('active');
 			this.spinning = false;
-		} else if (!this.spinning && this.activities > 0) {
+		} else if (!this.spinning && this.count > 0) {
 			this.spinner.classList.add('active');
 			this.spinning = true;
 		}
 	}
 	
-	startActivity(){
-		this.activities++;
+	startActivity(stopActivityCallback){
+		let activityId = this.nextId++;
+		
+		this.activities[activityId] = stopActivityCallback;
+		this.count++;
+		
 		this.updateSpinner();
+		
+		return activityId;
 	}
 	
-	endActivity(){
-		this.activities--;
+	// Force stop an ongoing activity
+	stopActivity(activityId) {
+		console.log('Force stopping activity ID: ' + activityId);
 		
-		if (this.activities < 0) {
-			throw 'Activity counter fell below 0. Kit broke the app, again.';
-		}
+		this.activities[activityId]();
+		
+		this.endActivity(activityId);
+	}
+	
+	stopAllActivities() {
+		Object.keys(this.activities).forEach(function(activityId) {
+			this.stopActivity(activityId);
+		}, this);
+	}
+	
+	// Indicates activity has ended of its own accord
+	endActivity(activityId){
+		delete this.activities[activityId];
+		this.count--;
 		
 		this.updateSpinner();
 	}
