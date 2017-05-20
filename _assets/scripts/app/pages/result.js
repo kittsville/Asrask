9613a23649cdc7265e6c81d9e91e3dfa5bc0687b
Kit <kittsville@googlemail.com>
2017-05-20 20:38:25 +0100

    Add rendering of brigading users table

--- _assets/scripts/app/pages/result.js
+++ _assets/scripts/app/pages/result.js
@@ -1,10 +1,33 @@
 #= require app/pages
 
 class resultPage extends appPage {
-	constructor(names) {
-		super();
+	constructor(wrapper, activityManager, names) {
+		super(wrapper, activityManager);
 		
 		this.resultId = names.resultId;
+		
+		this.loadPage();
+	}
+	
+	loadPage() {
+		this.activityId = this.activityManager.startActivity(function(){console.log('stopped');});
+		
+		let uri = 'results/' + this.resultId,
+		params  = {
+			success : this.renderPage.bind(this),
+		},
+		req = apiRequest(uri, params);
+	}
+	
+	renderPage(response) {
+		console.log(response);
+		
+		let generator = new tableGenerator(),
+		table         = generator.genUserTable(response.data);
+		
+		this.wrapper.appendChild(table);
+		
+		this.activityManager.endActivity(this.activityId);
 	}
 	
 	static get route() {
--- _assets/scripts/app/template.js
+++ _assets/scripts/app/template.js
@@ -1,2 +1,132 @@
-console.log('Templator Loaded');
-
+class tableGenerator {
+	constructor() {
+		this.nonNum      = 'mdl-data-table__cell--non-numeric';
+		this.userBase    = 'https://reddit.com/user/';
+		this.commentBase = 'https://reddit.com/r/';
+	}
+	
+	_getNumericHeader() {
+		return document.createElement('th');
+	}
+	
+	_getTextHeader() {
+		let header = this._getNumericHeader();
+		
+		header.className = this.nonNum;
+		
+		return header;
+	}
+	
+	_genNumericCell() {
+		return document.createElement('td');
+	}
+	
+	_genTextCell() {
+		let cell = this._genNumericCell();
+		
+		cell.className = this.nonNum;
+		
+		return cell;
+	}
+	
+	_subCell(participation) {
+		let cell = this._genNumericCell();
+		
+		cell.textContent = (participation / 10) + '%';
+		
+		return cell;
+	}
+	
+	genUserTable(data) {
+		let body = document.createElement('tbody'),
+		table    = document.createElement('table');
+		
+		table.className = 'mdl-cell mdl-data-table mdl-js-data-table mdl-shadow--2dp mdl-cell--6-col brigading-users';
+		
+		// HEAD
+		let head = document.createElement('thead'),
+		hRow     = document.createElement('tr'),
+		subRow   = document.createElement('tr'),
+		username = this._getTextHeader(),
+		subs     = this._getTextHeader(),
+		alpha    = this.subHeader(data.alpha),
+		beta     = this.subHeader(data.beta),
+		comments = this._getTextHeader();
+		
+		var linkBase = this.commentBase + data.alpha.name + '/comments/' + data.thread + '//';
+		
+		username.rowSpan   = 2;
+		comments.rowSpan   = 2;
+		subs.colSpan       = 2;
+		
+		username.classList.add('username');
+		subs.classList.add('participation');
+		comments.classList.add('comments');
+		
+		username.textContent = 'Username';
+		subs.textContent     = 'Participation';
+		comments.textContent  = 'Comments';
+		
+		hRow.appendChild(username);
+		hRow.appendChild(subs);
+		hRow.appendChild(comments);
+		
+		subRow.appendChild(alpha);
+		subRow.appendChild(beta);
+		
+		head.appendChild(hRow);
+		head.appendChild(subRow);
+		
+		table.appendChild(head);
+		
+		// BODY
+		data.entries.forEach(function(entry) {
+			let row  = document.createElement('tr'),
+			user     = entry.thing,
+			username = this._genTextCell(),
+			alpha    = this._subCell(entry.alpha_participation),
+			beta     = this._subCell(entry.beta_participation),
+			comments = this._genTextCell(),
+			userLink = document.createElement('a');
+			
+			userLink.href        = this.userBase + user.name;
+			userLink.textContent = '/u/' + user.name;
+			
+			let last = entry.comments.length - 1;
+			
+			entry.comments.forEach(function(commentId, index) {
+				var commentLink         = document.createElement('a');
+				commentLink.href        = linkBase + commentId + '/?context=99';
+				commentLink.textContent = commentId;
+				
+				comments.appendChild(commentLink);
+				
+				if (index != last) {
+					let separator = document.createTextNode(', ');
+					
+					comments.appendChild(separator);
+				}
+			}, this);
+			
+			username.appendChild(userLink);
+			row.appendChild(username);
+			row.appendChild(alpha);
+			row.appendChild(beta);
+			row.appendChild(comments);
+			
+			body.appendChild(row);
+		}, this);
+		
+		table.appendChild(body);
+		
+		return table;
+	}
+	
+	subHeader(subreddit) {
+		let header = this._getNumericHeader();
+		
+		header.textContent = '/r/' + subreddit.name;
+		
+		return header;
+	}
+}
