203b8453111cc4fc383d5cd3edeebff561655ab0
Kit <kittsville@googlemail.com>
2017-05-28 18:12:43 +0100

    Add reversable notifications
    Notifications can provide an 'Undo' callback to reverse the action the
    user has been notified of.
    #12

--- _assets/scripts/app/notification.js
+++ _assets/scripts/app/notification.js
@@ -1,17 +1,36 @@
 class notificationManager {
 	constructor(snackbarElement) {
-		this.snackbar = snackbarElement.MaterialSnackbar;
+		this.snackbar      = snackbarElement.MaterialSnackbar;
+		this.undoCallbacks = {}; // Keeps tracks of which undo callback have been called
+		this.nextId        = 0;
 	}
 	
-	addNotification(message, undoCallback = null) {
+	addNotification(message) {
 		let data = {
 			message : message,
 		};
 		
-		if (undoCallback) {
-			data.actionHandler = undoCallback;
-			data.actionText    = 'Undo';
-		}
+		this.snackbar.showSnackbar(data);
+	}
+	
+	addReversableNotification(message, undo) {
+		let callbackId = this.nextId++,
+		callback = function() {
+			let alreadyCalled = this.undoCallbacks[callbackId];
+			
+			if (!alreadyCalled) {
+				this.undoCallbacks[callbackId] = true;
+				
+				undo();
+			}
+		}.bind(this),
+		data = {
+			message       : message,
+			actionHandler : callback,
+			actionText    : 'Undo',
+		};
+		
+		this.undoCallbacks[callbackId] = false;
 		
 		this.snackbar.showSnackbar(data);
 	}
