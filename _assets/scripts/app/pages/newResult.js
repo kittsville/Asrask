3930eadff05653d4959a596ec063c3decafc7537
Kit <kittsville@googlemail.com>
2017-10-16 16:37:08 +0100

    Fix new result form layout
    Grouped all form elements under form tag because it's basic good practice and so pressing Enter within text fields causes form submision
    #10

--- _assets/scripts/app/pages/newResult.js
+++ _assets/scripts/app/pages/newResult.js
@@ -10,10 +10,10 @@ class NewResultPage extends AppPage {
 
 	renderPage() {
 		// Form Introduction
-		let card  	= cE('div', 'mdl-color--white mdl-shadow--4dp mdl-color-text--grey-800 mdl-card mdl-cell--8-col'),
-		title     	= cE('div', 'mdl-card__title'),
-		titleText 	= cE('h2',  'mdl-card__title-text'),
-		helpText  	= cE('div', 'mdl-card__supporting-text');
+		let card  = cE('div', 'mdl-color--white mdl-shadow--4dp mdl-color-text--grey-800 mdl-card mdl-cell--8-col'),
+		title     = cE('div', 'mdl-card__title'),
+		titleText = cE('h2',  'mdl-card__title-text'),
+		helpText  = cE('div', 'mdl-card__supporting-text');
 
 		titleText.textContent = 'New Result';
 		helpText.innerHTML  = 'Identify users brigading a thread from a subreddit';
@@ -24,22 +24,25 @@ class NewResultPage extends AppPage {
 
 		// Form Data Entry
 		let formGenerator = new FormGenerator(this.navManager),
-		form               = cE('form'),
-		threadInput        = formGenerator.textInput('threadUrl', 'Thread URL'),
-		subInput           = formGenerator.textInput('subName', "Name of brigading subreddit");
-
-		form.appendChild(threadInput);
-		form.appendChild(subInput);
-		card.appendChild(form);
-
-		// Form Submission
-		let submitWrapper = cE('div', 'mdl-card__actions mdl-card--border'),
+		fieldWrap         = cE('div', 'field-wrap'),
+		form              = cE('form'),
+		submitWrapper     = cE('div', 'mdl-card__actions mdl-card--border'),
 		submitButton      = cE('button', 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored');
 
+		this.threadInput  = formGenerator.textInput('threadUrl', 'Thread URL');
+		this.subInput     = formGenerator.textInput('subName', "Name of brigading subreddit");
+
+		fieldWrap.appendChild(this.threadInput);
+		fieldWrap.appendChild(this.subInput);
+		form.appendChild(fieldWrap);
+
 		submitButton.textContent = 'Analyse Thread';
+		submitButton.type        = 'submit';
 
 		submitWrapper.appendChild(submitButton);
-		card.appendChild(submitWrapper);
+		form.appendChild(submitWrapper);
+
+		card.appendChild(form);
 
 		this.setPage(card);
 	}
